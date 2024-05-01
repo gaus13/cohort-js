449f7eb610a0187db6434fc508ddf3bca8b9311d
gaus13 <gulammgauss@gmail.com>
2024-05-01 22:05:24 +0530

    first commit

---
+++ week1/one.js
@@ -0,0 +1,82 @@
+// const age = [22, 24, 23,34, 12 , 3];
+// const numofpeople = age.length;
+
+// for( let i=0; i< numofpeople; i++){
+//     if(age[i] % 2 == 0){
+//         console.log(age[i]);
+//     }
+// }
+
+// function greet(){
+//     console.log("hello danish");
+// }
+
+// setTimeout(greet, 3*1000)
+// //setTimeout delays the printing of
+// //hello by 3 secs
+
+//gives string length
+// const abs="aaalllu";
+// console.log(abs.length)
+
+// //the indexOf function gives index of target
+// function findindex(str, target){
+//     console.log("original string:", str)
+//     console.log("index:", str.indexOf(target));
+// }
+//  findindex("hello world", "world");
+
+//replace a string
+// const str= "hello javascript"
+// console.log(str.replace("hello", "js"))
+
+// //splits a string into small pieces or arrays
+// const value = "hi,my,name,is,danish"
+// const words = value.split(",");
+// console.log(words);
+
+//trim function delete the spaces 
+//toUpperCase to capital letters
+// toLowerCase to small letters
+
+
+//shift and unshift put and remove elements from the 
+//front of an array 
+
+//merge to array
+// const initialArray = [1, 2, 3];
+// const secondArray = [4, 5 , 6];
+// console.log(initialArray.concat(secondArray));
+
+//  const initialArray = [1, 2, 3];
+//  function logthing(str){
+//     console.log(str + " is a string");
+
+//  }
+//  initialArray.forEach(logthing)
+
+// //class and object in js
+// class animal{
+//     constructor(name, legcount, speaks){
+//         this.name= name;
+//         this.legcount= legcount
+//         this.speaks=speaks;
+//     }
+
+//     speak(){
+//         console.log("hi there" + this.speaks);
+//     }
+    
+// legs(){
+//     console.log("hi there" + this.legcount);
+// }
+
+// }
+
+
+// let dog= new animal("dog", 4, "bhow bhow"); object bnana
+// dog.speak();
+// dog.legs(); call kiya fun ko on object
+
+//parse and json function in js
+
