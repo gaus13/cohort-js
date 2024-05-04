fffb775634474190017e9965643edbc5f5af27bf
gaus13 <gulammgauss@gmail.com>
2024-05-04 17:02:19 +0530

    second learning

---
+++ week1/two.js
@@ -0,0 +1,31 @@
+
+// promise syntax
+// var d= new Promise(function(resolve){
+//     resolve(aalu");
+// });
+// function callback(){
+//     console.log(d);
+// }
+// d.then(callback)
+
+
+//asyn await let us get rid of .then syntax of promises
+//example:
+// function myasync(){
+//     let p= new Promise(function(resolve){
+//         setTimeout(function() {
+//             resolve("hi danish")
+          
+//         },2000)
+     
+// });
+// console.log(p);
+//   return p;
+// }
+
+// async function main(){
+//     let aalu =  await myasync();
+//     console.log("hi gaus");
+// }
+
+// main();
